266677ef588868ca997d6d7f5d93fdd6f30639f9
MelvinRadebe <melizweradebe@gmail.com>
2023-12-22 10:28:23 +0200

    Exercise 2.1

--- src/vector/size_capacity.cpp
+++ src/vector/size_capacity.cpp
@@ -5,7 +5,14 @@
 #include <iostream>
 
 using namespace std;
+// Vector capacity is the amount of memory it constains
+// vectors size is the contigous data that is stored in it
 
+
+//The size of a vector refers to the number of elements it currently contains.
+//It represents the actual count of elements that have been added to the vector.
+
+//The capacity of a vector, on the other hand, is the amount of space allocated to store elements.
 int main()
 {
     vector<int> vec;
